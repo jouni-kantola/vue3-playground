a3eec613761e80057a33cc387a388c7bb34e87cc
Jouni Kantola <jouni@kantola.se>
2022-04-15 00:10:14 +0200

    Inline image

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import InlineImage from './components/InlineImage.vue'
+</script>
+
+<template>
+  <InlineImage alt="Vue logo" src="/src/assets/logo.png" />
+</template>
---
+++ src/components/InlineImage.vue
@@ -0,0 +1,20 @@
+<template>
+  <img :src="blob" :alt="alt" />
+</template>
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import { useFetchBlob } from '../fetch'
+
+const props = defineProps({
+  src: {
+    type: String,
+    required: true
+  },
+  alt: {
+    type: String,
+    required: true
+  }
+})
+
+const { blob, error } = useFetchBlob(props.src)
+</script>
