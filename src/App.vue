83c42e5df1b558255473e5ac94482bab699d23b7
Jouni Kantola <jouni@kantola.se>
2022-04-15 13:56:25 +0200

    Render inline SVG

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import InlineImage from "./components/InlineImage.vue";
+import InlineSvg from "./components/InlineSvg.vue";
 </script>
 
 <template>
   <InlineImage alt="Vue logo" src="/src/assets/logo.png" />
+  <InlineSvg src="/src/assets/vue-logo.svg" />
 </template>
---
+++ src/components/InlineSvg.vue
@@ -0,0 +1,23 @@
+<template>
+  <Blob :src="src">
+    <template #default="{ blob }">
+      <div v-html="decodeDataUrl(blob)"></div>
+    </template>
+    <template #error="{ message }">
+      <p>{{ message }}</p>
+    </template>
+  </Blob>
+</template>
+
+<script setup lang="ts">
+import Blob from "./Blob.vue";
+
+defineProps({
+  src: {
+    type: String,
+    required: true,
+  },
+});
+
+const decodeDataUrl = (url: string) => atob(url.split("base64,")[1]);
+</script>
