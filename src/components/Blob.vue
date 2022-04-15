f62f350b621420e0ccd0b552fea43dadb7481c3e
Jouni Kantola <jouni@kantola.se>
2022-04-15 13:55:06 +0200

    Higher order component for fetching blobs

---
+++ src/components/Blob.vue
@@ -0,0 +1,17 @@
+<template>
+  <slot v-if="blob" :blob="blob" />
+  <slot v-if="error" name="error" :message="error" />
+</template>
+
+<script setup lang="ts">
+import { useFetchBlob } from "../fetch";
+
+const props = defineProps({
+  src: {
+    type: String,
+    required: true,
+  },
+});
+
+const { blob, error } = useFetchBlob(props.src);
+</script>
